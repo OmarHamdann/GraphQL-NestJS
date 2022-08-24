ae3bf5192c76c5fb44922f4e02ebb0ea22cab779
OmarHamdann <omar.haamdan@gmail.com>
2022-08-24 11:48:12 +0300

    add service to get all pets

--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { Pet } from './pet.entity';
 
 @Injectable()
-export class PetsService {}
+export class PetsService {
+    private readonly pets: Pet[];
+    
+    constructor() {
+        this.pets = [
+        {
+            id: 1,
+            name: 'Max',
+            type: 'dog',
+        },
+        {
+            id: 2,
+            name: 'Tom',
+            type: 'cat',
+        },
+        {
+            id: 3,
+            name: 'Simba',
+            type: 'cat',
+        },
+        ];
+    }
+    
+    async findAll(): Promise<Pet[]> {
+        return Promise.resolve(this.pets);
+    }
+    
+    findOne(id: number): Pet {
+        return this.pets.find(pet => pet.id === id);
+    }
+}
