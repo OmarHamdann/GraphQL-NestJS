4eb0d799925849ae2c9cbffd0ab195a2cf0557bf
OmarHamdann <omar.haamdan@gmail.com>
2022-08-24 17:58:07 +0300

    add create pet method

--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -1,35 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreatePetInput } from 'src/dto/create-Pet.input';
+import { Repository } from 'typeorm';
 import { Pet } from './pet.entity';
 
 @Injectable()
 export class PetsService {
-    private readonly pets: Pet[];
+   // private readonly pets: Pet[];
     
-    constructor() {
-        this.pets = [
-        {
-            id: 1,
-            name: 'Max',
-            type: 'dog',
-        },
-        {
-            id: 2,
-            name: 'Tom',
-            type: 'cat',
-        },
-        {
-            id: 3,
-            name: 'Simba',
-            type: 'cat',
-        },
-        ];
-    }
+    // constructor() {
+    //     this.pets = [
+    //     {
+    //         id: 1,
+    //         name: 'Max',
+    //         type: 'dog',
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Tom',
+    //         type: 'cat',
+    //     },
+    //     {
+    //         id: 3,
+    //         name: 'Simba',
+    //         type: 'cat',
+    //     },
+    //     ];
+    // }
+
+    constructor(@InjectRepository(Pet) private readonly  petRepository: Repository<Pet>) {}
     
     async findAll(): Promise<Pet[]> {
-        return Promise.resolve(this.pets);
+        return await this.petRepository.find(); //select * from pet
     }
+
+    async createPet(createPetInput:CreatePetInput): Promise<Pet> {
+        const pet = this.petRepository.create(createPetInput); //newPet = new Pet ();new.name =new.type
+
+        return await this.petRepository.save(pet);//insert into pet (name, type) values (:name, :type)
+    }
+
     
-    findOne(id: number): Pet {
-        return this.pets.find(pet => pet.id === id);
-    }
+
 }
