cade3d511b8781153dd2d2b6d4dc2f7472a304be
OmarHamdann <omar.haamdan@gmail.com>
2022-08-24 12:09:41 +0300

    add resolver to get all pets

--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -1,4 +1,15 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver,Query } from '@nestjs/graphql';
+import { Pet } from './pet.entity';
+import { PetsService } from './pets.service';
 
 @Resolver()
-export class PetsResolver {}
+export class PetsResolver {
+
+    constructor(private petsService:PetsService) {}
+
+    @Query(returns => [Pet])
+    async pets(): Promise<any> {
+        return this.petsService.findAll();
+    }
+
+}
