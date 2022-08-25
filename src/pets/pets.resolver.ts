7ec1cb47bfcd8bc925aaa4ff711e219b2419bad2
OmarHamdann <omar.haamdan@gmail.com>
2022-08-25 10:11:48 +0300

    add get one resolver

--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver,Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver,Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { CreatePetInput } from 'src/dto/create-Pet.input';
 import { Pet } from './pet.entity';
 import { PetsService } from './pets.service';
@@ -12,6 +12,12 @@ export class PetsResolver {
     async pets(): Promise<any> {
         return this.petsService.findAll();
     }
+
+    @Query(returns => Pet)
+    async getPet(@Args("id",{type:()=> Int}) id: number): Promise<any> {
+        return this.petsService.findOne(id);
+    }
+
 
 
     @Mutation(returns => Pet)
