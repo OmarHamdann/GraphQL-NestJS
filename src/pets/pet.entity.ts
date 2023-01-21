acaf9699670406d5f61404d49527bbfa759b6c71
OmarHamdann <omar.haamdan@gmail.com>
2023-01-21 10:55:09 +0300

    fix pet entity

--- src/pets/pet.entity.ts
+++ src/pets/pet.entity.ts
@@ -1,29 +1,28 @@
-import { Field, Int, ObjectType } from "@nestjs/graphql";
-import { Owner } from "src/owners/entities/owner.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+/* eslint-disable prettier/prettier */
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Owner } from 'src/owners/entities/owner.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 @ObjectType()
 export class Pet {
   @PrimaryGeneratedColumn()
-  @Field(type=>Int)
+  @Field((type) => Int)
   id: number;
 
   @Column()
   @Field()
   name: string;
 
-  @Column({nullable:true})
-  @Field({nullable:true})
+  @Column({ nullable: true })
+  @Field({ nullable: true })
   type?: string;
 
   @Column()
-  @Field(type=>Int)
+  @Field((type) => Int)
   ownerId: number;
 
-  @ManyToOne(type=>Owner, owner=>owner.pets)
-  @Field(type=>[Owner],{nullable:true})
+  @ManyToOne((type) => Owner, (owner) => owner.pets)
+  @Field((type) => [Owner], { nullable: true })
   owners: Owner[];
-  
-
 }
