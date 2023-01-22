ff1f85c1380485e01e9731403c8f962ac4316c14
OmarHamdann <omar.haamdan@gmail.com>
2023-01-22 10:56:13 +0300

    fix pet resolver

--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -1,4 +1,12 @@
-import { Resolver,Query, Mutation, Args, Int, Parent, ResolveField } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  Int,
+  Parent,
+  ResolveField,
+} from '@nestjs/graphql';
 import { Owner } from 'src/owners/entities/owner.entity';
 import { CreatePetInput } from 'src/pets/dto/create-Pet.input';
 import { Pet } from './pet.entity';
@@ -6,39 +14,32 @@ import { PetsService } from './pets.service';
 
 @Resolver((of) => Pet)
 export class PetsResolver {
+  constructor(private petsService: PetsService) {}
 
-    constructor(private petsService:PetsService) {}
+  @Query((returns) => [Pet])
+  async pets(): Promise<any> {
+    return this.petsService.findAll();
+  }
 
-    @Query(returns => [Pet])
-    async pets(): Promise<any> {
-        return this.petsService.findAll();
-    }
+  @Query((returns) => Pet)
+  async getPet(@Args('id', { type: () => Int }) id: number): Promise<any> {
+    return this.petsService.findOne(id);
+  }
 
-    @Query(returns => Pet)
-    async getPet(@Args("id",{type:()=> Int}) id: number): Promise<any> {
-        return this.petsService.findOne(id);
-    }
+  @Mutation((returns) => Pet)
+  async createPet(
+    @Args('createPetInput') createPetInput: CreatePetInput,
+  ): Promise<any> {
+    return this.petsService.createPet(createPetInput);
+  }
 
+  @ResolveField((returns) => Owner)
+  owner(@Parent() pet: Pet): Promise<Owner> {
+    return this.petsService.getOwner(pet.ownerId);
+  }
 
-
-    @Mutation(returns => Pet)
-    async createPet(@Args("createPetInput") createPetInput:CreatePetInput): Promise<any> {
-        return this.petsService.createPet(createPetInput);
-    }
-
-
-    @ResolveField(returns => Owner)
-    owner(@Parent() pet: Pet): Promise<Owner> {
-        return this.petsService.getOwner(pet.ownerId);
-    }
-
-    @Mutation(() => Pet)
-     removePet(@Args("id",{type:()=> Int}) id: number) {
-   
-        return  this.petsService.remove(id);
-    }
-
-
-
-
+  @Mutation(() => Pet)
+  removePet(@Args('id', { type: () => Int }) id: number) {
+    return this.petsService.remove(id);
+  }
 }
