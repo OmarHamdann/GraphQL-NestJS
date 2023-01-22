be0de4f2d6f0e98fd707c53f338222ddd3bef45d
OmarHamdann <omar.haamdan@gmail.com>
2023-01-22 10:56:46 +0300

    fix pet service

--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -7,35 +7,31 @@ import { Pet } from './pet.entity';
 
 @Injectable()
 export class PetsService {
+  constructor(
+    @InjectRepository(Pet) private readonly petRepository: Repository<Pet>,
+    private readonly ownersService: OwnersService,
+  ) {}
 
-    constructor(@InjectRepository(Pet) private readonly  petRepository: Repository<Pet>,private readonly ownersService: OwnersService) {}
-    
-    async findAll(): Promise<Pet[]> {
-        return await this.petRepository.find(); //select * from pet
-    }
+  async findAll(): Promise<Pet[]> {
+    return await this.petRepository.find(); //select * from pet
+  }
 
-    async findOne(id: number): Promise<Pet> {
-        return await this.petRepository.findOneOrFail({where: {id}}); //select * from pet where id = :id
-    }
+  async findOne(id: number): Promise<Pet> {
+    return await this.petRepository.findOneOrFail({ where: { id } }); //select * from pet where id = :id
+  }
 
+  async createPet(createPetInput: CreatePetInput): Promise<Pet> {
+    const pet = this.petRepository.create(createPetInput); //newPet = new Pet ();new.name =new.type
 
-    async createPet(createPetInput:CreatePetInput): Promise<Pet> {
-        const pet = this.petRepository.create(createPetInput); //newPet = new Pet ();new.name =new.type
+    return await this.petRepository.save(pet); //insert into pet (name, type) values (:name, :type)
+  }
 
-        return await this.petRepository.save(pet);//insert into pet (name, type) values (:name, :type)
-    }
+  async getOwner(id: number): Promise<any> {
+    return await this.ownersService.findOne(id);
+  }
 
-    async getOwner(id: number): Promise<any> {
-        return await this.ownersService.findOne(id);
-    }
-
-    //delete from pet where id = :id
-    async remove(id: number): Promise<any> {
-        
-        return await this.petRepository.delete(id);
-    }
-
-
-
-
+  //delete from pet where id = :id
+  async remove(id: number): Promise<any> {
+    return await this.petRepository.delete(id);
+  }
 }
